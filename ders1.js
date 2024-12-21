607fb6642b502411a7e53199db316f18fa9c3ec9
Batuakbulut <akbulut_batu@hotmail.com>
2024-12-21 16:56:40 +0300

    ekrana cıktı verme,değişkenler ,emel veri tipleri

---
+++ ders1.js
@@ -0,0 +1,32 @@
+// alert("marhaba dünya")
+// console.log("merhaba dünya 2")
+// document.write("hello 3")
+// var degisken = "ritim"
+// console.log(degisken)
+
+// degisken = "batu-erhan"
+// console.log(degisken)
+
+//veri tipleri (ilkel)
+var d1 = 20//tamsayı (intenger)
+var d2 = 3.14//ondalıksayı(float)
+var d3 = "hello"//dize/metin (string)
+
+console.log(typeof d3) //değişkenin tiplerini verir
+
+var sayi1 = 50
+var sayi2 = 13
+
+
+console.log(sayi1 + sayi2)
+console.log(sayi1 / sayi2)
+console.log(sayi1 - sayi2)
+console.log(sayi1 * sayi2)
+
+//temel opareratörler
+console.log(5 ** 3)
+console.log(53 % 2)//bölümden kalan
+console.log(++sayi1)
+console.log(--sayi1)
+// kendimizle ilgili bilgileri konsos ekranına yazdıran program
+//örneğin adınız yaşınız soyadınız hobileriniz doğum tarihimiz not html ye bağlamayı unutma

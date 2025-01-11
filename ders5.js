7349b862b06f47350b68ebf9fc5cce14ef59406e
batu akbulut <akbulut_batu@hotmail.com>
2025-01-11 15:06:39 +0300

    fıfo ve lıfo yapısı

---
+++ ders5.js
@@ -0,0 +1,18 @@
+var cars = ["volvo", "bmw", "ford", "mazda"]
+
+//cars.push() sonuna eleman akleme
+//cars.pop() son eleman sil
+
+//Array in başına eleman eklemek ve silmek daha maliyetli
+//cars.unshift("audi");ilk eleman ekle
+//cars.shift(); ilk eleman sil
+
+console.log(cars)
+
+//fıfo (frist in frist out) -ilk giren ilk çıkar (deque -kuyruk)
+cars.push()//eklemek için
+cars.shift()//çıkarmak için
+
+//lıfo (last ın frist out) -son giren ilk çıkar (stack)
+cars.unshift()//eklemek için 
+cars.pop//çıkarmak için
